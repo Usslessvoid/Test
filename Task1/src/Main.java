f1dd325894262296f612fdb125dd98d77415df92
Usslessvoid <36482404+Usslessvoid@users.noreply.github.com>
2018-10-03 19:48:46 +0300

    initial commit

---
+++ Task1/src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+import by.training.task1.Operator;
+
+public class Main {
+	static final String NUMBERS = "1234567890.";
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter expression:");
+		double answer = calculate(scanner.nextLine());
+		System.out.println(answer);
+	}
+	public static double calculate(String str) {
+		int start = 0;
+		int end = 0;
+		boolean gap;
+		StringBuilder builder = new StringBuilder(str);
+		for(Operator action: Operator.values()) {
+			gap = true;
+			for(int position = 0; position<builder.length();position++) {
+				char current = builder.charAt(position);
+				if(isDigit(current)) {            //detect begin of number before
+					if(gap) {
+						gap=false;
+						start = position;
+					}
+				}else {
+					gap = true;
+					if(current == action.getSymbol()) {   //find operator
+						double firstOperand;
+						double secondOperand;
+						boolean begin = false;
+						for(int tmp = position+1; tmp<builder.length();tmp++) { //detect end of number
+							if(isDigit(builder.charAt(tmp))) {
+								begin = true;
+								end = tmp;
+							}else if(begin) {
+								break;
+							}	
+						}
+						firstOperand = doubleFromString(builder.substring(start, position));
+						secondOperand = doubleFromString(builder.substring(position+1, end+1));
+						builder.replace(start, end+1, String.valueOf(action.solve(firstOperand, secondOperand)));
+						start = 0;
+						gap = false;
+
+					}
+				}
+			}
+		}
+		return doubleFromString(builder.toString());
+	}
+	public static double doubleFromString(String builder) {
+		double result = 0;
+		try {
+			result = Double.parseDouble(builder);
+		}catch (NumberFormatException e) {
+			System.err.println("Wrong expression format!");
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public static boolean isDigit(char c) {
+		return NUMBERS.contains(String.valueOf(c));
+	}
+
+
+}
